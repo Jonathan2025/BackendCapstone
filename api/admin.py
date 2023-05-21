59bacf19dd5e5cbcc98693a059d3e798dbfa098e
jonathan2025 <Jonathan.Liang75@outlook.com>
2023-05-21 11:53:57 -0400

    register the models we created on the django admin panel

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+# In other words, if we want to use our models in the admin panel then we need to register them
+
+from .models import UserProfile, Comment, Post, Location
+
+admin.site.register(UserProfile)
+admin.site.register(Comment)
+admin.site.register(Post)
+admin.site.register(Location)
